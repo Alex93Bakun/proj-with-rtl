4dfa88be50052374a173f40fd136cf1e4b3effc3
Alex93Bakun <56162141+Alex93Bakun@users.noreply.github.com>
2021-09-20 21:30:21 +0300

    render App component #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Search = ({ value, onChange, children }) => (
+    <div>
+        <label htmlFor="search">{children}</label>
+        <input
+            id="search"
+            type="text"
+            value={value}
+            onChange={onChange}
+            placeholder="search text..."
+        />
     </div>
-  );
-}
+);
+
+const App = () => {
+    const [search, setSearch] = useState('');
+
+    const handleChange = ({ target }) => {
+        setSearch(target.value);
+    };
+
+    return (
+        <div>
+            <img src='' alt='search image' />
+            <Search value={search} onChange={handleChange}>
+                Search:
+            </Search>
+            <p>Searches for {search ? search : '...'}</p>
+        </div>
+    );
+};
 
 export default App;
